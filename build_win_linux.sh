26599f6cef1c189fc322f428c09555dc7b449f61
dcryans <david.cryans@dynatrace.com>
2023-11-29 16:13:43 -0500

    change name - monaco -> OneTopology

--- build_win_linux.sh
+++ build_win_linux.sh
@@ -1,3 +1,4 @@
-env GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -a -tags netgo -ldflags '-X github.com/dynatrace/dynatrace-configuration-as-code/pkg/version.MonitoringAsCode=2.x -w -extldflags "-static"' -o ./build/monaco-windows-amd64.exe ./cmd/monaco
-env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -a -tags netgo -ldflags '-X github.com/dynatrace/dynatrace-configuration-as-code/pkg/version.MonitoringAsCode=2.x -w -extldflags "-static"' -o ./build/monaco-linux-amd64 ./cmd/monaco
-cp -p ./build/monaco-linux-amd64 ./bin/monaco
+env GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -a -tags netgo -ldflags '-X github.com/dynatrace/dynatrace-configuration-as-code/pkg/version.MonitoringAsCode=2.x -w -extldflags "-static"' -o ./build/one-topology-windows-amd64.exe ./cmd/monaco
+env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -a -tags netgo -ldflags '-X github.com/dynatrace/dynatrace-configuration-as-code/pkg/version.MonitoringAsCode=2.x -w -extldflags "-static"' -o ./build/one-topology-linux-amd64 ./cmd/monaco
+cp -p ./build/one-topology-linux-amd64 ./bin/one-topology
+cp -p ./build/one-topology-linux-amd64 ./bin/monaco
